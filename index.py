c734d383314b6eec08ca65e2309713eb4d7d9dfd
Treasure237-ux <itreasure237@gmail.com>
2025-03-28 20:16:00 +0100

    my python programming week2 assignment

--- index.py
+++ index.py
@@ -13,18 +13,18 @@ print(f'After append: {my_list}')
 my_list[1] = 15
 print(my_list)
 
-#Extend my_list with another list: [50, 60, 70]
+#Extend my_list with another list: [50, 60, 70]
 list = [50, 60, 70]
 my_list.extend(list)
 print(my_list)
 
-#Remove the last element from my_list.
+#Remove the last element from my_list.
 del my_list[6]
 print(my_list)
 
-#Sort my_list in ascending order.
+#Sort my_list in ascending order.
 my_list.sort()
 print(my_list)
 
-#Find and print the index of the value 30 in my_list
+#Find and print the index of the value 30 in my_list
 print(my_list.index(30))
