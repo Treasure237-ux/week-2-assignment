a9d6608a846985f5047d02df049ae4d79e5d71e4
Treasure237-ux <itreasure237@gmail.com>
2025-03-28 02:48:07 +0100

    my python programming week2 assignment

---
+++ index.py
@@ -0,0 +1,30 @@
+#create an empty list
+my_list = []
+print(f'Before append: {my_list}')
+
+#append:10,20,30,40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(f'After append: {my_list}')
+
+#insert 15 at the second position in the list
+my_list[1] = 15
+print(my_list)
+
+#Extend my_list with another list: [50, 60, 70]
+list = [50, 60, 70]
+my_list.extend(list)
+print(my_list)
+
+#Remove the last element from my_list.
+del my_list[6]
+print(my_list)
+
+#Sort my_list in ascending order.
+my_list.sort()
+print(my_list)
+
+#Find and print the index of the value 30 in my_list
+print(my_list.index(30))
